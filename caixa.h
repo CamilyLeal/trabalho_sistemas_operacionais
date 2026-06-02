71a67c41b2de122d64b2b4c67fc83b3e8de9951b
CamilyLeal <camilyleals080@gmail.com>
2026-06-02 18:53:22 -0300

    Structs de pessoa e caixa

---
+++ caixa.h
@@ -0,0 +1,18 @@
+#ifndef CAIXA_H
+#define CAIXA_H
+
+#include <pthread.h>
+#include "pessoa.h"
+
+#define MAX_PESSOAS 8
+
+typedef struct {
+    pthread_mutex_t mutex; //apenas uma thread por vez
+    pthread_cond_t cond[MAX_PESSOAS]; //variável de condição
+    Pessoa *fila[MAX_PESSOAS]; //fila de espera
+    int ocupado; //0 ou 1
+    int tamanho_fila;
+    int ordem;
+} Caixa;
+
+#endif
---
+++ pessoa.h
@@ -0,0 +1,21 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+typedef enum {
+    COMUM = 0,
+    DEFICIENTE = 1,
+    IDOSO = 2,
+    GRAVIDA = 3
+} Prioridade;
+
+typedef struct {
+    int id;
+    char nome[20];
+
+    Prioridade prioridade_inicial;
+    Prioridade prioridade_atual;
+
+    int frustracoes;
+    int ordem_chegada;
+} Pessoa;
+#endif
